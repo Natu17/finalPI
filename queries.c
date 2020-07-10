123ff5996c508c33fd1f3155cafa846ad715dd23
arfernandez2000 <67968160+arfernandez2000@users.noreply.github.com>
2020-07-10 19:40:38 -0300

    Update and added query3 (tested)

--- queries.c
+++ queries.c
@@ -10,7 +10,6 @@ typedef struct elemType {
 	float number;
 } elemType;
 
-
 typedef struct node {
 	elemType elem;
 	struct node* tail;
@@ -23,60 +22,96 @@ typedef struct list {
 	PNode current;
 } list;
 
+typedef list * PList;
 
-static PNode addListRec(PNode first, char * name, float number) {
-	int c;
+PNode addListRec(PNode first, char * name, float number) {
 	errno = 0;
-	if(first == NULL ||   first->elemType.number - number < 0){
+	if(first == NULL ||   first->elem.number - number < 0){
 		PNode aux = malloc(sizeof(node));
 		if(errno == ENOMEM) { 
 			perror("Not enough memory");
 			return first;
 		}
-		aux->elemType.name = malloc(strlen(name) + 1);
+		aux->elem.name = malloc(strlen(name) + 1);
 		if(errno == ENOMEM) {
 			perror("Not enough memory");
 			return first;
 		}
-		strcpy(aux->elemType.name, name);
-		aux->elemType.number = number;
+		strcpy(aux->elem.name, name);
+		aux->elem.number = number;
 		aux->tail = first;
 		return aux;
 	}
-	first->tail = addRec(first->tail, name, number);
+	first->tail = addListRec(first->tail, name, number);
 	return first;
 }
 
-void add(list list, char* name, float number){
-	list->first = addRec(list->first, name, number);
+void add(PList list, char* name, float number){
+	list->first = addListRec(list->first, name, number);
 }
 
-void toBeginList(list list) {
+void toBeginList(PList list) {
 	list->current = list->first;
 }
 
-int hasNextList(list list) {
+int hasNextList(PList list) {
 	return list->current != NULL;
 }
 
-elemType * nextList(list list) {
+elemType * nextList(PList list) {
 	errno = 0;
-	if(!hasNext(list)) {
+	if(!hasNextList(list)) {
 		perror("There are no more elements");
 		return NULL;
 	}
 	elemType * ans = malloc(sizeof(elemType));
 	if(errno == ENOMEM) {
 		perror("Not enough memory");
-		return first;
+		return NULL;
 	}
-	ans->number = list->current->list.diameter;
-	ans->name = malloc(strlen(list->current->elemType.name) + 1);
+	ans->number = list->current->elem.number;
+	ans->name = malloc(strlen(list->current->elem.name) + 1);
 	if(errno == ENOMEM) {
 		perror("Not enough memory");
-		return first;
+		return NULL;
 	}
-	strcpy(ans->name, list->current->elemType.name);
+	strcpy(ans->name, list->current->elem.name);
 	list->current = list->current->tail;
 	return ans;
 }
+
+void freeListRec(PNode first) { 
+	if(first != NULL) {
+		free(first->elem.name);
+		freeListRec(first->tail);
+	}
+	free(first);
+}
+
+void freeList(PList list) {
+	freeListRec(list->first);
+	free(list);
+}
+
+void query3(treeADT tree){
+	PList listQ3 = calloc(1, sizeof(list));
+	FILE * file;
+	file = fopen ("./query3.csv", "w+");
+	toBeginTree(tree);
+	while(hasNextTree(tree)) {
+		treeType * treeAux = nextTree(tree);
+		add(listQ3, treeAux->name, treeAux->diameter);	
+		free(treeAux->name);
+		free(treeAux);
+	}
+	toBeginList(listQ3);
+	while(hasNextList(listQ3)) {
+		elemType * ans = nextList(listQ3);
+		fprintf(file, "%s;%.2f\n", ans->name, ans->number);
+		free(ans->name);
+		free(ans);
+	}
+	freeList(listQ3);
+	fclose(file);
+}
+
